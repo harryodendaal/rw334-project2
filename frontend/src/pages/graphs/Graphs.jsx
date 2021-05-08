72302ac2bfa81a2af59c3c34863e7bb3855bdea9
Stephanus Rutger Van Huyssteen <23052228@sun.ac.za>
2021-05-08 11:38:25 +0000

    Replace Graphs.jsx

--- frontend/src/pages/graphs/Graphs.jsx
+++ frontend/src/pages/graphs/Graphs.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import {Graph} from './Graph'
+import styled from './Graph.module.css'
 
 export const Graphs = () => {
   const [visType, setVisType] = useState('social-network');
 
   return (
-    <div>
+    <div class={styled.btn}>
       <button onClick={() => setVisType('social-network')}>Social Network</button>
       <button onClick={() => setVisType('label-propagation')}>Label propagation</button>
       <button onClick={() => setVisType('shortest-path')}>Shortest path</button>
